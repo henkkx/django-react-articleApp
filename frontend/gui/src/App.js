5cfc299875aa7ea6078e600a932d94276dcb8177
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-04-09 18:32:33 +0100

    added routes, and detaile article view

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom'
+import BaseRouter from './routes'
 import './App.css';
 import 'antd/dist/antd.css';
 import CustomLayout from './containers/Layout';
 import ArticleListView from './containers/ArticleListVIew';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CustomLayout>
-          <ArticleListView/>
-        </CustomLayout>
+        <Router>
+          <CustomLayout>
+            <ArticleListView/>
+          </CustomLayout>
+        </Router>
       </div>
     );
   }
