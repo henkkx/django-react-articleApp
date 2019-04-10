66a35b113214a02e00f8c42f82cc815157b5e648
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-04-10 14:24:17 +0100

    created update and delete api views

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, include
 
 
-from .views import ArticleListView, ArticleDetailView
+from .views import(
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    UpdateAPIView,
+    DestroyAPIView
+)
 
 urlpatterns = [
     path('', ArticleListView.as_view()),
+    path('create/', ArticleCreateView.as_view()),
     path('<pk>', ArticleDetailView.as_view()),
+    path('<pk>/update/', ArticleDetailView.as_view()),
+    path('<pk>/delete/', ArticleDetailView.as_view()),
 
 ]
